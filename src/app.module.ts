7df0f5b12d1a5fe15dfb856d8cf501d24d8b6714
inhyeok <ss112d86@gmail.com>
2022-04-14 19:34:00 +0900

    Feat: WebRTC P2P 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WebRtcGateway } from './gateway/web-rtc.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WebRtcGateway],
 })
 export class AppModule {}
