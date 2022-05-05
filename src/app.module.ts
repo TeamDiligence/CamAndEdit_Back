2a3aea3240ea9ad4493042ca5bcbb3ddfcf0996e
inhyeok <ss112d86@gmail.com>
2022-05-05 18:59:56 +0900

    Feat: Config Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebRtcGateway } from './gateway/web-rtc.gateway';
+import AppConfig from './global/config/AppConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `${__dirname}/global/config/env/.${process.env.NODE_ENV}.env`,
+      ],
+      load: [AppConfig],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, WebRtcGateway],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import AppConfig from './global/config/AppConfig';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(AppConfig.KEY) private config: ConfigType<typeof AppConfig>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
   }
